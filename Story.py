589a34f93986079d1c32be7ff67f09be2ab4aae1
VTam25 <vivienne.tam2020@gmail.com>
2018-02-18 10:12:51 -0500

    Add files via upload

---
+++ Story.py
@@ -0,0 +1,23 @@
+
+#Blueprint 2018!!!
+
+#This is a program for a choose your own adventure story for safety when going out
+
+from sys import exit()
+
+
+def start():
+    print("Welcome!)
+    print("What is your name?")
+    print("You and your friends are planning to explore Cambridge and the MIT campus for a fun time!! It is two hours before you planned to meet up.")
+
+def robber_end():
+    print("")
+
+def kidnap_end():
+    print("")
+
+def win_end():
+    print("")
+
+start()
